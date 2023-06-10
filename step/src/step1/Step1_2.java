cac1b3bbd75afac92879892722ac44c4890adad4
campanula01 <97ehflehfl1@naver.com>
2023-06-10 10:53:39 +0900

    Step1

---
+++ step/src/step1/Step1_2.java
@@ -0,0 +1,15 @@
+package step1;
+
+import java.util.*;
+/*두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.*/
+public class Step1_2 {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		int A=input.nextInt();
+		int B=input.nextInt();
+		
+		System.out.print(A+B);
+		
+	}
+
+}
---
+++ step/src/step1/Step1_4.java
@@ -0,0 +1,23 @@
+package step1;
+
+/*문제
+두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A×B를 출력한다.*/
+import java.util.*;
+
+public class Step1_4 {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		
+		int A=input.nextInt();
+		int B=input.nextInt();
+		
+		System.out.print(A*B);
+	}
+
+}
